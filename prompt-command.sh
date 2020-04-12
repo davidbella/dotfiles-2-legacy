ad8ed623a35a839a3a633be3a275f1872dc33e9f
David Bella <davidhbella@gmail.com>
2020-04-12 10:28:02 -0700

    Add prompt-command.sh

---
+++ prompt-command.sh
@@ -0,0 +1,36 @@
+# /bin/bash
+
+if [ $TERM == "tmux-256color" ]
+then
+  tmux_cwd="$(tmux display-message -p -F "#{pane_current_path}" -t0)"
+
+  directory_name="$(echo $tmux_cwd | rev | cut -d"/" -f1 | rev)"
+  up_directory_name="$(echo $tmux_cwd | rev | cut -d"/" -f2 | rev)"
+  my_documents_dir_name="$(echo $tmux_cwd | cut -d"/" -f4,5)"
+  my_documents_cwd="$(echo $tmux_cwd | cut -d"/" -f6)"
+
+  output=""
+
+  case "$up_directory_name" in
+    home)
+      output="__home"
+      ;;
+    *)
+      case "$my_documents_dir_name" in
+        Documents/MyDocuments)
+          output=$my_documents_cwd
+
+          case "$up_directory_name" in
+            ManualMD)
+              output=$directory_name
+              ;;
+            Documents)
+              output=$directory_name
+              ;;
+          esac
+          ;;
+      esac
+  esac
+
+  tmux rename-session $output 2>/dev/null
+fi
