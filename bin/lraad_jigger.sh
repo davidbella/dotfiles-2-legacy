90edee11af56b3883cb0659712c910697fe359ec
David Bella <davidhbella@gmail.com>
2021-11-28 21:25:43 -0800

    Clean up bad bin scripts

--- bin/lraad_jigger.sh
+++
@@ -1,7 +0,0 @@
-netctl stop lraad
-
-sleep 2
-netctl start lraad
-
-sleep 10
-systemctl restart mullvad-daemon
--- bin/wired_jigger.sh
+++
@@ -1,7 +0,0 @@
-netctl stop wired
-
-sleep 2
-netctl start wired
-
-sleep 10
-systemctl restart mullvad-daemon
