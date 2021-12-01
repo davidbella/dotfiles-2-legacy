2d479dc80b3abdc486b14f4f8e189e80966d2e7c
David Bella <davidhbella@gmail.com>
2021-12-01 11:36:48 -0800

    Updated orgmode projects and templates

--- .config/nvim/lua/org.lua
+++ .config/nvim/lua/org.lua
@@ -23,9 +23,22 @@ require('orgmode').setup({
     '~/Documents/MyDocuments/Orgmode/orgmode/*',
     '~/Documents/MyDocuments/ManualMD/orgmode/*',
     '~/Documents/MyDocuments/Budget/orgmode/*',
+    '~/Documents/MyDocuments/MyDocumentsWeb/orgmode/*',
+    '~/Documents/MyDocuments/Neovim/orgmode/*',
   },
   org_default_notes_file = '~/Documents/MyDocuments/Orgmode/orgmode/refile.org',
-  org_agenda_templates = { t = { description = 'Task', template = '* TODO %?\n  %T' } },
+  org_agenda_templates = {
+    t = {
+      description = 'Task',
+      template = '* TODO %?\n  %T',
+      target = '~/Documents/MyDocuments/ManualMD/orgmode/manualmd.org',
+    },
+    b = {
+      description = 'Bookmark',
+      template = '* [[%?][]]\n:PROPERTIES:\n:CREATED: %U\n:END:\n\n',
+      target = '~/Documents/MyDocuments/ManualMD/orgmode/bookmarks.org',
+    }
+  },
   -- org_agenda_start_on_weekday = 0,
   org_todo_keywords = {'TODO(t)', 'DEFD(f)', '|', 'DONE(d)'}
 })
