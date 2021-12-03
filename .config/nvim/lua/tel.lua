d858d0973f5dcfcbdbbd9c02ed33f48f7a42822d
David Bella <davidhbella@gmail.com>
2021-12-03 10:15:44 -0800

    Update telescope: sort and delete buffers, consolidate theme

--- .config/nvim/lua/tel.lua
+++ .config/nvim/lua/tel.lua
@@ -1,23 +1,22 @@
 local actions = require('telescope.actions')
 
-local picker_theme = {
-  theme = "ivy",
-  layout_config = {
-    height = 15,
-  },
-  border = true,
-
-  borderchars = {
-    "z",
-    prompt = { "─", " ", " ", " ", "─", "─", " ", " " },
-    results = { " " },
-    preview = { "─", " ", " ", " ", "─", "─", " ", " " },
-  }
-}
-
 require('telescope').setup{
   defaults = {
+    theme = "ivy",
+    layout_strategy = "bottom_pane",
+    layout_config = {
+      height = 15,
+    },
+    border = true,
+
+    borderchars = {
+      "z",
+      prompt = { "─", " ", " ", " ", "─", "─", " ", " " },
+      results = { " " },
+      preview = { "─", " ", " ", " ", "─", "─", " ", " " },
+    },
     file_sorter = require("telescope.sorters").get_generic_fuzzy_sorter, -- gief me regex
+    sorting_strategy = "ascending",
     mappings = {
       i = {
         ["<C-j>"] = actions.move_selection_next,
@@ -30,12 +29,17 @@ require('telescope').setup{
     }
   },
   pickers = {
-    find_files = picker_theme,
-    live_grep = picker_theme,
-    grep_string = picker_theme,
-    buffers = picker_theme,
-    lsp_document_symbols = picker_theme,
-    lsp_references = picker_theme,
+    buffers = {
+      mappings = {
+        i = {
+          ["<c-d>"] = actions.delete_buffer,
+        },
+        n = {
+          ["d"] = actions.delete_buffer,
+        }
+      },
+      sort_lastused = true
+    },
   },
 }
 
@@ -62,6 +66,4 @@ map('n', '<leader>f', "<cmd>lua files()<cr>", options)
 map('n', '<leader>g', "<cmd>lua grep()<cr>", options)
 map('n', '<leader>b', "<cmd>lua buffers()<cr><Esc>", options)
 map('n', '<leader>v', "<cmd>lua lsp()<cr><Esc>", options)
-
-map('n', '<leader>F', "<cmd>lua require('telescope.builtin').grep_string()<cr>", options)
-map('n', '<leader>]', "<cmd>lua require('telescope.builtin').lsp_definitions()<cr><Esc>", options)
+map('n', '<leader>G', "<cmd>lua require('telescope.builtin').grep_string()<cr>", options)
